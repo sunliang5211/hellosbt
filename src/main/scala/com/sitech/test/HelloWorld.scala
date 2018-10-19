1dac7f3c7ae4349f06c14983698fdc46d2291727
sunliang5211 <sunliang5211@163.com>
2018-10-19 10:33:15 +0800

    Initial commit

---
+++ src/main/scala/com/sitech/test/HelloWorld.scala
@@ -0,0 +1,14 @@
+package com.sitech.test
+
+/**
+  * Created by Thinkpad on 2018/10/19.
+  */
+object HelloWorld {
+
+  def main(args: Array[String]): Unit = {
+    println("hello sunliang")
+    println("hello yanyitong")
+    println("hello world")
+  }
+
+}
